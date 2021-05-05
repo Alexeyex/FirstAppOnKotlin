b7e758c28778a005f2acf3ba62c69adcc60043c5
Алексей МОП Чермянин <iamalive_spb@mail.ru>
2021-05-05 16:57:08 +0300

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.4.32"
+}
+
+group = "ru.netology"
+version = "1.5-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
